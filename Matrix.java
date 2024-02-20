151d3d15d1351251f0e045486c956f896a0859ae
Barmaley34 <128833613+Barmaley34@users.noreply.github.com>
2024-02-20 12:00:40 +0300

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Matrix {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Random random = new  Random();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите количество строк матрицы: ");
